1c43f2fd0f2cf380b739385b6db355f1618dcadf
Justin Lilly <justin@justinlilly.com>
2010-02-26 02:21:44 -0500

    Adding list views to the context of annotation list views.

--- codereview/apps/annotation/views.py
+++ codereview/apps/annotation/views.py
@@ -1,8 +1,19 @@
+import datetime
 from django.contrib.contenttypes.models import ContentType
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from annotation.models import Annotation
 from annotation.forms import AnnotationModelForm
+from django.utils import simplejson
+
+def annotation_list(request, content_type_str, obj_id, position):
+    ct = ContentType.objects.get(name=content_type_str)
+    obj = ct.model_class().objects.get(id=obj_id)
+    ann_list = Annotation.objects.filter(content_type=ct, object_id=obj.id)
+    if request.is_ajax():
+        return HttpResponse(simplejson.dumps(list(ann_list)))
+    return render_to_response('annotations/list.html',
+                              {'annotation_list': ann_list})
 
 def annotate_form(request, content_type_str, obj_id, position=None):
     """
@@ -10,9 +21,11 @@ def annotate_form(request, content_type_str, obj_id, position=None):
     """
     ct = ContentType.objects.get(name=content_type_str)
     obj = ct.model_class().objects.get(id=obj_id)
+    ann_list = Annotation.objects.filter(content_type=ct, object_id=obj.id, position=position)
     preform_data = {'commentor':request.user.id,
                     'content_type':ct.id,
                     'object_id':obj_id,
+                    'date_added':datetime.datetime.now(),
                     'position':position}
     f = AnnotationModelForm(request.POST or None, initial=preform_data)
     if f.is_valid():
@@ -25,6 +38,7 @@ def annotate_form(request, content_type_str, obj_id, position=None):
                                        'content_type': ct.name,
                                        'obj_id': obj.id,
                                        'position': position,
+                                       'annotation_list': ann_list,
                                        'annotation_object': obj})
     else:
         # invalid / new
@@ -33,4 +47,5 @@ def annotate_form(request, content_type_str, obj_id, position=None):
                                    'content_type': ct.name,
                                    'obj_id': obj.id,
                                    'position': position,
+                                   'annotation_list': ann_list,
                                    'annotation_object': obj})

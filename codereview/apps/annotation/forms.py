f6f003da54585a2e8dc281aa8735e9717de7306f
Justin Lilly <justin@justinlilly.com>
2010-02-26 02:17:58 -0500

    Form widgets, save the comment, should be hidden.

--- codereview/apps/annotation/forms.py
+++ codereview/apps/annotation/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.forms import widgets
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from annotation.models import Annotation
 
 class AnnotationModelForm(forms.ModelForm):
+    commentor = forms.ModelChoiceField(widget=widgets.HiddenInput, queryset=User.objects.all())
+    content_type = forms.ModelChoiceField(widget=widgets.HiddenInput, queryset=ContentType.objects.all())
+    object_id = forms.IntegerField(widget=widgets.HiddenInput)
+    date_added = forms.DateTimeField(widget=widgets.HiddenInput, required=False)
+    position = forms.CharField(widget=widgets.HiddenInput)
     class Meta:
         model = Annotation
